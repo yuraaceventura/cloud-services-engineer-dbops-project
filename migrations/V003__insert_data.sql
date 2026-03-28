a5663519aa8ed33af8ecc65347f8cd0d372825c0
Yuriy Alekseev <yuraaceventura@gmail.com>
2026-03-28 20:19:06 +0300

    removed comment

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -1,4 +1,3 @@
-# Заполним таблицы
 INSERT INTO product (id, name, picture_url, price) VALUES (1, 'Сливочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg',  320.00);
 INSERT INTO product (id, name, picture_url, price) VALUES (2, 'Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00);
 INSERT INTO product (id, name, picture_url, price) VALUES (3, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00);
